8c9a2af2d308db2583ea8f13d117cafcf2c61c64
majurageerthan <majuran.thasan@gmail.com>
2023-02-28 10:54:37 +0530

    Updated time format without minutes and sec

--- src/utils/dateTimeHelpers.js
+++ src/utils/dateTimeHelpers.js
@@ -1,11 +1,12 @@
 import { DateTime } from 'luxon';
 
+// https://moment.github.io/luxon/#/formatting?id=table-of-tokens
 const DATE_TIME_FORMAT = 'dd-MM-yyyy HH:mm:ss';
-
+const DATE_TIME_FORMAT_2 = "dd-MM-yyyy ':' hh a";
 const DATE_ONLY_TIME_FORMAT = 'dd-MM-yyyy';
 
 export const getCurrentReadableFileNameTimeStamp = () => DateTime.now().toFormat(DATE_TIME_FORMAT);
 
-export const getReadableTimeStampFromEpoch = (epochMs) => DateTime.fromMillis(epochMs).toFormat(DATE_TIME_FORMAT);
+export const getReadableTimeStampFromEpoch = (epochMs) => DateTime.fromMillis(epochMs).toFormat(DATE_TIME_FORMAT_2);
 
 export const getEpochFromDateString = (dateString) => DateTime.fromFormat(dateString, DATE_ONLY_TIME_FORMAT).toMillis();
