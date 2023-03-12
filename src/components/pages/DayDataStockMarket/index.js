d029d79c3a7bed81dd924e0f9aa4a581a37f44b3
majurageerthan <majuran.thasan@gmail.com>
2023-03-12 18:23:02 +0530

    Added share volume and trade volume meta data

--- src/components/pages/DayDataStockMarket/index.js
+++ src/components/pages/DayDataStockMarket/index.js
@@ -12,7 +12,7 @@ import {
 import { Line } from 'react-chartjs-2';
 import { doc, getDoc } from 'firebase/firestore/lite';
 import { STOCK_DB_FIREBASE_FIRE_STORE } from '../../../utils/constants';
-import { getEpochFromDateString, getReadableTimeStampFromEpoch } from '../../../utils/dateTimeHelpers';
+import { getEpochFromDateString } from '../../../utils/dateTimeHelpers';
 import LoadingAnimationCenter from '../../atom/LoadingAnimationCenter';
 import { fireStoreDbFirebase } from '../../../utils/firebaseHelper';
 
@@ -39,16 +39,26 @@ const options = {
   },
 };
 
+const STOCKS_META_DATA_FIRESTORE = {
+  HIGH_PRICE: 'highPrice',
+  LOW_PRICE: 'lowPrice',
+  SHARES_VOLUME: 'shareVolume',
+  TRADE_VOLUME: 'tradeVolume',
+};
+
 const ACTION_TYPES_DAILY_DATA = {
   HIGH_PRICE: 'HIGH_PRICE',
   LOW_PRICE: 'LOW_PRICE',
-  SHARES: 'SHARES',
+  SHARES_VOLUME: 'SHARES_VOLUME',
+  TRADE_VOLUME: 'TRADE_VOLUME',
+  DATE_ONLY: 'DATE_ONLY',
 };
 
 const TITLES_DAILY_DATA = {
   HIGH_PRICE: 'HIGH PRICE',
   LOW_PRICE: 'LOW PRICE',
-  SHARES: 'SHARES',
+  SHARES_VOLUME: 'SHARES VOLUME',
+  TRADE_VOLUME: 'TRADE VOLUME',
 };
 
 const defaultDataSetState = [
@@ -67,7 +77,7 @@ const defaultDataSetState = [
 ];
 
 const dispatchDailyStocksReducer = (state, action) => {
-  switch (action.type) {
+  switch (action?.type) {
     case ACTION_TYPES_DAILY_DATA.HIGH_PRICE: return [
       ...state.filter((data) => data.label !== TITLES_DAILY_DATA.HIGH_PRICE),
       {
@@ -86,56 +96,112 @@ const dispatchDailyStocksReducer = (state, action) => {
         backgroundColor: 'rgba(53, 162, 235, 0.5)',
       },
     ];
-    case ACTION_TYPES_DAILY_DATA.SHARES: return [
-      ...state.filter((data) => data.label !== TITLES_DAILY_DATA.SHARES),
+
+    default:
+      return defaultDataSetState;
+  }
+};
+
+const dispatchLabelsDailyStocksReducer = (state, action) => {
+  switch (action?.type) {
+    case ACTION_TYPES_DAILY_DATA.SHARES_VOLUME: return [
+      ...state.filter((data) => data.label !== TITLES_DAILY_DATA.SHARES_VOLUME),
       {
-        label: TITLES_DAILY_DATA.SHARES,
-        data: action.shares,
+        label: TITLES_DAILY_DATA.SHARES_VOLUME,
+        data: action.shareVolume,
         borderColor: 'rgb(255, 99, 132)',
         backgroundColor: 'rgba(255, 99, 132, 0.5)',
       },
     ];
+
+    case ACTION_TYPES_DAILY_DATA.DATE_ONLY:
+      return [...action.data];
     default:
-      return state;
+      return defaultDataSetState;
   }
 };
 
 const DayDataStockMarket = ({ selectedCompany, isLoading }) => {
   const [dailyStocksDataSetState, dispatchDailyStocksDataSet] = useReducer(dispatchDailyStocksReducer, defaultDataSetState);
-  const [dailyStocksDataSetLabel, setDailyStocksDataSetLabel] = useState({});
+  const [dailyStocksDataSetLabel, setDailyStocksDataSetLabel] = useState([]);
+  const [shareVolume, setShareVolume] = useState({});
+  const [tradeVolume, setTradeVolume] = useState({});
 
-  const addHighPrice = (highPrice) => {
-    dispatchDailyStocksDataSet({
-      type: ACTION_TYPES_DAILY_DATA.HIGH_PRICE,
-      highPrice,
-    });
-  };
-
-  const isStockDataAvailable = dailyStocksDataSetLabel?.length && dailyStocksDataSetState?.length;
+  const isStockDataAvailable = dailyStocksDataSetLabel?.length && dailyStocksDataSetState.find((stockData) => stockData?.data?.length > 0);
 
   useEffect(() => {
     isLoading(!isStockDataAvailable);
-  }, [selectedCompany]);
+    console.log('dailyStocksDataSetState', dailyStocksDataSetState);
+    // console.log('dailyStocksDataSetState:notZero', dailyStocksDataSetState.find((stockData) => stockData?.data?.length > 0));
+  }, [selectedCompany, dailyStocksDataSetLabel, dailyStocksDataSetState]);
 
   useEffect(() => {
-    console.log('initStockMetaData:Document useEffect:');
+    console.log('shareVolume:tradeVolume', shareVolume, tradeVolume);
 
-    const initStockMetaData = async () => {
-      const docRef = doc(fireStoreDbFirebase, `${STOCK_DB_FIREBASE_FIRE_STORE}/${selectedCompany.id}/${selectedCompany.name}`, 'highPrice');
+    const newKeysWithMetaData = dailyStocksDataSetLabel?.map((key) => {
+      const keyOfLabels = key?.[0];
+      const shareVolumeLabel = shareVolume[keyOfLabels] ?? '';
+      const tradeVolumeLabel = tradeVolume[keyOfLabels] ?? '';
+      return [keyOfLabels, `SV: ${shareVolumeLabel}`, `TV: ${tradeVolumeLabel}`];
+    });
+    console.log('newKeysWithMetaData', newKeysWithMetaData);
+    setDailyStocksDataSetLabel(newKeysWithMetaData);
+  }, [shareVolume, tradeVolume]);
+
+  useEffect(() => {
+    const addHighPrice = (highPrice) => {
+      console.log('addHighPrice', highPrice);
+      dispatchDailyStocksDataSet({
+        type: ACTION_TYPES_DAILY_DATA.HIGH_PRICE,
+        highPrice,
+      });
+    };
+
+    const addLowPrice = (lowPrice) => {
+      console.log('addLowPrice', lowPrice);
+      dispatchDailyStocksDataSet({
+        type: ACTION_TYPES_DAILY_DATA.LOW_PRICE,
+        lowPrice,
+      });
+    };
+
+    console.log('initStockMetaData:Document useEffect:');
+    dispatchDailyStocksDataSet();
+    const initStockMetaData = async (stockMetaDataValue) => {
+      const docRef = doc(fireStoreDbFirebase, `${STOCK_DB_FIREBASE_FIRE_STORE}/${selectedCompany.id}/${selectedCompany.name}`, stockMetaDataValue);
       const docSnap = await getDoc(docRef);
       if (docSnap.exists()) {
         const data = docSnap.data();
-        console.log('initStockMetaData:Document data:', data);
+        console.log(`initStockMetaData:Document data:${stockMetaDataValue}: `, data);
         const keys = Object.keys(data).sort((a, b) => getEpochFromDateString(a) - getEpochFromDateString(b));
-        setDailyStocksDataSetLabel(keys);
+        const keysWithMetaData = keys.map((key) => [key, 'SV:', 'TV:']);
+        setDailyStocksDataSetLabel(keysWithMetaData);
         console.log(`label: ${keys}`);
         options.plugins.title.text = selectedCompany.name;
-        addHighPrice(keys.map((key) => data[key]));
+        const pricesInkeyOrder = keys.map((key) => data[key]);
+        switch (stockMetaDataValue) {
+          case STOCKS_META_DATA_FIRESTORE.HIGH_PRICE:
+            addHighPrice(pricesInkeyOrder);
+            break;
+          case STOCKS_META_DATA_FIRESTORE.LOW_PRICE:
+            addLowPrice(pricesInkeyOrder);
+            break;
+          case STOCKS_META_DATA_FIRESTORE.SHARES_VOLUME:
+            setShareVolume(data);
+            break;
+          case STOCKS_META_DATA_FIRESTORE.TRADE_VOLUME:
+            setTradeVolume(data);
+            break;
+          default:
+            break;
+        }
       }
     };
 
     if (selectedCompany?.id) {
-      initStockMetaData();
+      Object.keys(STOCKS_META_DATA_FIRESTORE).forEach((stockMetaData) => {
+        initStockMetaData(STOCKS_META_DATA_FIRESTORE[stockMetaData]);
+      });
     }
   }, [selectedCompany]);
 
